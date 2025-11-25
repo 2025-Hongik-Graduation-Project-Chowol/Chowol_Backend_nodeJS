4559c8ae991707c10ff9fc719422a031a9e49476
sumin020 <128468746+sumin020@users.noreply.github.com>
2025-11-26 04:00:59 +0900

    update cors

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from "cors";
 import router from './src/routes/router.js';
 import errorHandler from './src/handlers/errorHandler.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+
+const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 app.get('/', (req, res) => {
